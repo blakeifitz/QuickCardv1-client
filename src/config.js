97182da69cc563f5fcb07fbb9516cada69d20cf8
blakeifitz <blakeifitz@gmail.com>
2021-01-06 23:21:13 -0500

    another typo

--- src/config.js
+++ src/config.js
@@ -1,11 +1,10 @@
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    API_ENDPOINT: `https://secure-harbor-52275.herokuapp.com/apihttps://secure-harbor-52275.herokuapp.com/api`,
+    API_ENDPOINT: `https://cors-anywhere.herokuapp.com/https://secure-harbor-52275.herokuapp.com/api`,
     API_KEY: process.env.REACT_APP_API_KEY,
     TOKEN_KEY: 'quickcard-client-auth-token',
   }
 
 
 
-  
