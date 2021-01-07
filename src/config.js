5df05bba83c0beb09262ab07117055a7be43370f
blakeifitz <blakeifitz@gmail.com>
2021-01-06 23:58:52 -0500

    local server

--- src/config.js
+++ src/config.js
@@ -1,10 +1,7 @@
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    API_ENDPOINT: `https://secure-harbor-52275.herokuapp.com/api`,
+    API_ENDPOINT: `http://localhost:8000`,
     API_KEY: process.env.REACT_APP_API_KEY,
     TOKEN_KEY: 'quickcard-client-auth-token',
   }
-
-
-
