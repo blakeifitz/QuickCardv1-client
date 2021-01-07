fa7533b81fcd038bd797a2f473cc30b618f2faa4
blakeifitz <blakeifitz@gmail.com>
2021-01-06 23:31:30 -0500

    revert

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    API_ENDPOINT: `https://cors-anywhere.herokuapp.com/https://secure-harbor-52275.herokuapp.com/api`,
+    API_ENDPOINT: `https://secure-harbor-52275.herokuapp.com/api`,
     API_KEY: process.env.REACT_APP_API_KEY,
     TOKEN_KEY: 'quickcard-client-auth-token',
   }
