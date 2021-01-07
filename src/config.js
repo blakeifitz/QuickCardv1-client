1fbc971407c25a7c37793dc94d696e49de337a1a
blakeifitz <blakeifitz@gmail.com>
2021-01-06 23:15:53 -0500

    cors anywhere

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    API_ENDPOINT: `postgres://zduneydppypegc:28ff8918428848ec1904e81b1e6734f5af9a59ff65be80b3a30f21bb84f68e3e@ec2-52-205-61-60.compute-1.amazonaws.com:5432/d26vmappkaoo5h`,
+    API_ENDPOINT: `https://cors-anywhere.herokuapp.com/https://secure-harbor-52275.herokuapp.com/api`,
     API_KEY: process.env.REACT_APP_API_KEY,
     TOKEN_KEY: 'quickcard-client-auth-token',
   }
